b060f2c2f53eef9793889cf66422cba7ca9e9c86
TrileshNaidu <trileshnaidukatreddy@gmail.com>
2025-07-03 10:13:05 +0530

    Enhanced user management features

--- src/main/java/com/example/usermanagementapi/model/User.java
+++ src/main/java/com/example/usermanagementapi/model/User.java
@@ -4,10 +4,25 @@ import jakarta.persistence.*;
 import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.Size;
+import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.annotations.UpdateTimestamp;
+
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.sql.Timestamp;
+
 
 @Entity
 @Table(name = "users")
 public class User {
+    //New: Creation Timestamp
+    @Column(name="created at",updatable = false)//updatable = false ensures it's set only once
+    @CreationTimestamp
+    private LocalDate createdAt;
+    //new: Updated timestamp
+    @Column(name ="updated At")
+    @UpdateTimestamp
+    private LocalDateTime updatedAt;
     @Id //specifies the primary key of the entity
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
@@ -19,6 +34,22 @@ public class User {
     @NotBlank(message = "Email is required")
     @Email(message = "Email should be valid")
     private String email;
+
+    public LocalDate getCreatedAt() {
+        return createdAt;
+    }
+
+    public void setCreatedAt(LocalDate createdAt) {
+        this.createdAt = createdAt;
+    }
+
+    public LocalDateTime getUpdatedAt() {
+        return updatedAt;
+    }
+
+    public void setUpdatedAt(LocalDateTime updatedAt) {
+        this.updatedAt = updatedAt;
+    }
 
     public User(){
 
